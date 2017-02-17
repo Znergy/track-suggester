c4324977ac2f99897cbbb81f61a5841e3bd1e3b8
Ryan <ryanjonesirl@gmail.com>
2017-02-17 14:52:27 -0800

    add the logic to take the user values and show the correlating div

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+/* Things to do..
+* 1) take input values save as global variable
+* 2) range those values on button click and show req. div
+*/
+
+/* Notes:
+- 
+
+*/
+
+$(document).ready(function() {
+  
+  var choiceTotal = 0;
+  
+  $("#submit").on('click', function() {
+    
+    // take the values passed when clicked and when the if else runs we will have the correlating div showing..
+    
+    
+    
+    if (choiceTotal <= 6) {
+      $("#designTrack").show();
+    } else if (choiceTotal > 6 && choiceTotal <= 12) {
+      $("#phpTrack").show();
+    } else if (choiceTotal > 12 && choiceTotal <= 18) {
+      $("#rubyTrack").show();
+    } else if (choiceTotal > 18 && choiceTotal <= 24) {
+      $("#androidTrack").show();
+    } else if (choiceTotal > 24 && choiceTotal <= 45) {
+      $("#cNetTrack").show();
+    }
+    
+  });
+});
